495a5a5bc91583da3d2440eb0a2dc023de08bbcb
JustRubik <danghuyhieu1434@gmail.com>
2026-05-17 14:02:58 +0700

    remove color_col_94

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -16,7 +16,7 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- paste over highlight word
 vim.keymap.set("x", "<leader>p", '"_dP')
-vim.opt.colorcolumn = "94"
+-- vim.opt.colorcolumn = "94"
 vim.opt.clipboard = "unnamedplus"
 -- fk llm-ls
 local notify_original = vim.notify
