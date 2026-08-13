4a0d23e5218570d820f7a9f331de0867d89607f7
JustRubik <danghuyhieu1434@gmail.com>
2026-08-13 11:02:25 +0700

    nothing special

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -8,7 +8,7 @@ vim.cmd("set relativenumber")
 vim.cmd("set cursorline")
 vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "white" })
 vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#ead84e" })
-vim.api.nvim_set_option("clipboard", "unnamed")
+vim.api.nvim_set_option("clipboard", "unnamedplus")
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 -- move selected lines
