7b924488f21bd6490bf60bf127ccc68f9312fc0a
JustRubik <danghuyhieu1434@gmail.com>
2026-08-28 16:08:10 +0700

    adjust NullLs and tabstop=2

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -4,7 +4,9 @@ return {
         local null_ls = require("null-ls")
         null_ls.setup({
             sources = {
-                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.stylua.with({
+                  command = vim.fn.stdpath("data") .. "/mason/bin/stylua",
+                }),
                 null_ls.builtins.formatting.prettier,
                 null_ls.builtins.formatting.black,
                 null_ls.builtins.formatting.isort,
